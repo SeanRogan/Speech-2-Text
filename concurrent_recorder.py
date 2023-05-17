4ec0d60f7f646def12d5cb86f0fb51a7accbec9a
Sean Rogan <74671024+SeanRogan@users.noreply.github.com>
2023-05-17 18:02:51 -0400

    adding perf counters and cli argument parsing to the recording scripts.

--- concurrent_recorder.py
+++ concurrent_recorder.py
@@ -5,17 +5,26 @@ import os
 import logging
 import concurrent.futures
 import time
+import argparse
 
-freq = 44100
-duration = 4  # in seconds
-recordings_dir = './recordings'  # Create the recordings directory if it doesn't exist
 logging.basicConfig(level=logging.DEBUG, filename='recorder_log.txt')
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--Directory', type=str, required=False, help='Configure the directory to which recordings are saved. Takes a string representing the file path where audio files are to be saved.')
+parser.add_argument('-s', '--Seconds', type=int, required =False, help='Configure the recording time, in seconds. Takes an integer, representing the length of recording interval. An argument of 7 will record a series of wav files 7 seconds long, until the script is stopped. Default setting is 5 seconds.')
+args = parser.parse_args()
+freq = 44100
+duration = 5  # recording duration, in seconds
+if args.Seconds is not None:
+    duration = args.Seconds
+recordings_dir = './recordings'
+if args.Directory is not None:
+    recordings_dir = args.Directory
 
 
 def record_audio():
     start = time.perf_counter()
     if not os.path.exists(recordings_dir):
-        os.makedirs(recordings_dir)
+        os.makedirs(recordings_dir)  # Create the recordings directory if it doesn't exist
     print('Recording')
     ts = datetime.datetime.now()  # create a timestamp to assign to audio files as they're made
     filename = ts.strftime("%H:%M:%S")  # Start recorder with the given values of duration and sample frequency
@@ -25,15 +34,19 @@ def record_audio():
     sd.wait()  # wait for recording to end before writing file
     end_ts = datetime.datetime.now()
     logging.debug('recording ended @' + end_ts.strftime('%H:%M:%S'))
-    file = (recording, filename)
-    end = time.perf_counter()
+    file = (recording, filename)  # create a tuple with the recording data and file name to be returned
+    end = time.perf_counter()  # perf test logging timer
     runtime = end - start
-    logging.info('recording loop ran: ' + str(runtime))
+    logging.info('recording loop runtime: ' + str(runtime))
     return file
 
 
 def write_wav_file(file):
-    wavio.write(f'{recordings_dir}/{file[1]}.wav', file[0], freq, sampwidth=2, clip='ignore')
+    start = time.perf_counter()
+    wavio.write(f'{recordings_dir}/{file[1]}.wav', file[0], freq, sampwidth=2, clip='ignore')   # write wav data to file
+    end = time.perf_counter()  # perf test logging timer
+    runtime = end - start
+    logging.info('file write loop runtime: ' + str(runtime))
 
 
 while True:
--- recorder.py
+++ recorder.py
@@ -3,17 +3,29 @@ import wavio
 import datetime
 import os
 import logging
+import time
+import argparse
 
-freq = 44100
-duration = 4  # in seconds
-recordings_dir = './recordings'
 logging.basicConfig(level=logging.DEBUG, filename='log.txt')
 
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', '--Directory', type=str, required=False, help='Configure the directory to which recordings are saved. Takes a string representing the file path where audio files are to be saved.')
+parser.add_argument('-s', '--Seconds', type=int, required =False, help='Configure the recording time, in seconds. Takes an integer, representing the length of recording interval. An argument of 7 will record a series of wav files 7 seconds long, until the script is stopped. Default setting is 5 seconds.')
+args = parser.parse_args()
+freq = 44100
+duration = 5  # recording duration, in seconds
+if args.Seconds is not None:
+    duration = args.Seconds
+recordings_dir = './recordings'
+if args.Directory is not None:
+    recordings_dir = args.Directory
 
 if not os.path.exists(recordings_dir):
     os.makedirs(recordings_dir)  # Create the recordings directory if it doesn't exist
+
 print('Recording')
 while True:
+    start = time.perf_counter()
     ts = datetime.datetime.now()  # create a timestamp to assign to audio files as they're made
     filename = ts.strftime("%H:%M:%S")  # Start recorder with the given values of duration and sample frequency
     logging.debug('recording started @:' + filename)
@@ -24,3 +36,6 @@ while True:
     logging.debug('recording ended @' + end_ts.strftime('%H:%M:%S'))
     wavio.write(f"./recordings/{filename}.wav", recording, freq, sampwidth=2,
                 clip="ignore")  # Convert the NumPy array to audio file
+    end = time.perf_counter()
+    rt = str(end - start)
+    logging.info(f'recording loop runtime for {duration}s: {rt}')
